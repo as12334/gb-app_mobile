8c153a0e9af8dff5beddbe96f087b0038bfbd167
fei <fei@dawoo.com>
2017-07-17 14:09:31 +0000

    ==移动端-新增站点188的图标和logo

--- mobile/java/so/wwb/gamebox/mobile/tools/AuthTool.java
+++ mobile/java/so/wwb/gamebox/mobile/tools/AuthTool.java
@@ -161,7 +161,7 @@ public class AuthTool {
         String[] ids = new String[]{
                 /*
                 "1,0001,开发一",
-                "21,rf80,测试01",*/
+                "21,rf80,测试01",
                 "71,8l6r,超博娱乐",
                 "76,XH5Z,澳门永利",
                 "110,cabu,超博娱乐",
@@ -218,7 +218,8 @@ public class AuthTool {
                 "183,nrpf,美高梅赌场",
                 "185,fyxi,Phoenix Gaming",
                 "186,cwad,赛博体育",
-                "187,b02h,澳门银河"
+                "187,b02h,澳门银河",*/
+                "188,acpb,鸿泰国际"
         };
 
         /* SELECT '"'||ss.id||','||ss.code||','||si."value"||'",' FROM sys_site ss LEFT JOIN site_i18n si ON ss."id" = si.site_id WHERE si.locale = 'zh_CN' AND si."type"='site_name' AND ss.status<>'2' AND ss.id > 183 order by ss.id */
