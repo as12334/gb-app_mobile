4e2434f23d4f806ab43bde3a99ef2d1bf8888361
mical <mical@dawoo.com>
2017-08-11 11:42:51 +0800

    修改试玩

--- mobile/java/DemoController.java
+++ mobile/java/DemoController.java
@@ -1,3 +1,4 @@
+import org.soul.commons.lang.SystemTool;
 import org.soul.model.gameapi.base.PlatformTypeEnum;
 import org.springframework.stereotype.Controller;
 import so.wwb.gamebox.model.master.player.vo.PlayerApiAccountVo;
@@ -12,6 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 public class DemoController extends so.wwb.gamebox.web.msites.controller.DemoController {
 
     public PlayerApiAccountVo doLogin(PlayerApiAccountVo playerApiAccountVo, HttpServletRequest request) {
+        StringBuilder domain = new StringBuilder();
+        domain.append(request.getServerName());
+        if (!domain.toString().contains("http")) {
+            domain.insert(0, "http://");
+        }
+        playerApiAccountVo.setLobbyUrl(domain.toString());
+        if (request.getHeader("User-Agent").contains("app_android")) {
+            playerApiAccountVo.setLobbyUrl("javascript:window.gb.finish()");
+        }
         playerApiAccountVo.setPlatformType(PlatformTypeEnum.mobile.getCode());
         return playerApiAccountVo;
     }
