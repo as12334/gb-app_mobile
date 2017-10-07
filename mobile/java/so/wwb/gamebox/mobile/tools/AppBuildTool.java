a1d5ee37d84756519ceee28ac35767589277bc87
fei <fei@dawoo.com>
2017-10-07 16:18:32 +0800

    ==移动端-站点238改为绿色主题

--- mobile/java/so/wwb/gamebox/mobile/tools/AppBuildTool.java
+++ mobile/java/so/wwb/gamebox/mobile/tools/AppBuildTool.java
@@ -136,7 +136,7 @@ public class AppBuildTool {
      */
     private static void getAppBuild(){
         String[] sites = new String[]{
-                "69,7cxt,一指通彩票",
+                /*"69,7cxt,一指通彩票",
                 "70,1wl5,天天彩票",
                 "71,8l6r,超博娱乐",
                 "76,XH5Z,澳门永利",
@@ -240,7 +240,9 @@ public class AppBuildTool {
                 "235,cspr,美高梅娱乐城",
                 "236,8gez,拉斯维加斯国际",
                 "802,98jb,凤凰彩票",
-                "237,akm1,金沙娱乐城"
+                "237,akm1,金沙娱乐城",*/
+                "238,wlf6,BET365",
+                "803,yg9x,头彩彩票"
         };
 
         /* SELECT '"'||ss.id||','||ss.code||','||si."value"||'",' FROM sys_site ss LEFT JOIN site_i18n si ON ss."id" = si.site_id WHERE si.locale = 'zh_CN' AND si."type"='site_name' AND ss.status<>'2' AND ss.id not in (75,80) AND ss.id > 183 order by ss.id; */
@@ -313,7 +315,7 @@ public class AppBuildTool {
 
     private static String setSiteType(Integer siteId) {
         String siteType = "integrated";
-        if (siteId == 69 || siteId == 70 || siteId == 197 || siteId == 800 || siteId == 801 || siteId == 802)
+        if (siteId == 69 || siteId == 70 || siteId == 197 || siteId == 800 || siteId == 801 || siteId == 802 || siteId == 803)
             siteType = "lottery";
         return siteType;
     }
@@ -323,13 +325,13 @@ public class AppBuildTool {
         if (siteId == 119 || siteId == 171) {
             theme = "blue.skin";
         }
-        else if (siteId == 141 || siteId == 161 || siteId == 207) {
+        else if (siteId == 141 || siteId == 161 || siteId == 207 || siteId == 238) {
             theme = "green.skin";
         }
         else if (siteId == 185) {
             theme = "pink.skin";
         }
-        else if (siteId == 69 || siteId == 70 || siteId == 197 || siteId == 800 || siteId == 801 || siteId == 802) {
+        else if (siteId == 69 || siteId == 70 || siteId == 197 || siteId == 800 || siteId == 801 || siteId == 802 || siteId == 803) {
             theme = "lottery.skin";
         }
         return theme;
@@ -339,11 +341,11 @@ public class AppBuildTool {
         try {
             if (siteId == 119 || siteId == 171) {
                 Files.copy(new File(from, "blue.skin"), new File(to, "blue.skin"));
-            } else if (siteId == 141 || siteId == 161 || siteId == 207) {
+            } else if (siteId == 141 || siteId == 161 || siteId == 207 || siteId == 238) {
                 Files.copy(new File(from, "green.skin"), new File(to, "green.skin"));
             } else if (siteId == 185) {
                 Files.copy(new File(from, "pink.skin"), new File(to, "pink.skin"));
-            } else if (siteId == 69 || siteId == 70 || siteId == 197 || siteId == 800 || siteId == 801 || siteId == 802) {
+            } else if (siteId == 69 || siteId == 70 || siteId == 197 || siteId == 800 || siteId == 801 || siteId == 802 || siteId == 803) {
                 Files.copy(new File(from, "lottery.skin"), new File(to, "lottery.skin"));
             }
         } catch (IOException ioe) {
