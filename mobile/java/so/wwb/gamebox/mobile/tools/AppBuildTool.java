0e09870be3f73cadd3b8463876a9cf93a9afcdc6
fei <fei@dawoo.com>
2017-10-07 16:27:07 +0800

    ==移动端-站点238改为绿色主题

--- mobile/java/so/wwb/gamebox/mobile/tools/AppBuildTool.java
+++ mobile/java/so/wwb/gamebox/mobile/tools/AppBuildTool.java
@@ -242,7 +242,7 @@ public class AppBuildTool {
                 "802,98jb,凤凰彩票",
                 "237,akm1,金沙娱乐城",*/
                 "238,wlf6,BET365",
-                "803,yg9x,头彩彩票"
+                "803,yg9x,头彩"
         };
 
         /* SELECT '"'||ss.id||','||ss.code||','||si."value"||'",' FROM sys_site ss LEFT JOIN site_i18n si ON ss."id" = si.site_id WHERE si.locale = 'zh_CN' AND si."type"='site_name' AND ss.status<>'2' AND ss.id not in (75,80) AND ss.id > 183 order by ss.id; */
