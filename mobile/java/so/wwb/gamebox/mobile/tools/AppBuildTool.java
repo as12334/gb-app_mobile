7c61b27b3e757f0e192c89d39c79eeba8453a59e
fei <fei@dawoo.com>
2017-10-21 22:07:05 +0800

    ==新增站点258的app构建脚本

--- mobile/java/so/wwb/gamebox/mobile/tools/AppBuildTool.java
+++ mobile/java/so/wwb/gamebox/mobile/tools/AppBuildTool.java
@@ -165,12 +165,13 @@ public class AppBuildTool {
                 "233,87lr,金沙娱乐城",        "235,cspr,美高梅娱乐城",      "236,8gez,拉斯维加斯国际",   "802,98jb,凤凰彩票",
                 "237,akm1,金沙娱乐城",        "238,wlf6,BET365",          "803,yg9x,头彩",           "239,5e7b,云顶娱乐",*/
 //                "805,yrxk,亿彩汇",
-                "251,e2ce,威尼斯人",
-                "252,pox4,COBO超博",
+//                "251,e2ce,威尼斯人",
+//                "252,pox4,COBO超博",
 //                "253,7rda,澳门威尼斯人",
 //                "255,p0a7,澳门威尼斯人",
 //                "256,vqwq,星博国际",
-                "257,wnmt,盛大国际",
+//                "257,wnmt,盛大国际",
+                "258,yqgk,金凯娱乐"
         };
 
         /* SELECT '"'||ss.id||','||ss.code||','||si."value"||'",' FROM sys_site ss LEFT JOIN site_i18n si ON ss."id" = si.site_id WHERE si.locale = 'zh_CN' AND si."type"='site_name' AND ss.status<>'2' AND ss.id not in (75,80) AND ss.id > 183 order by ss.id; */
@@ -409,7 +410,7 @@ public class AppBuildTool {
     public static void main(String[] args) {
         getAppBuild();
         try {
-            System.out.println("---android版本号加密：" + AesTool.encrypt("23", AppUpdate.KEY_UPDATE));
+            System.out.println("---android版本号加密：" + AesTool.encrypt("24", AppUpdate.KEY_UPDATE));
 //            System.out.println("---ios版本号加密：" + md5SysUserPermission("7", "ios"));
         } catch (Exception e) {
             e.printStackTrace();
