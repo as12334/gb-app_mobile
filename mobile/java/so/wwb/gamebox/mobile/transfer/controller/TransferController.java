26c6a1ae9e9d5c2f99665d0ad47e06341aee7b5f
cherry <cherry1012@mail.com>
2017-10-26 15:25:15 +0800

    ==盈利预警任务

--- mobile/java/so/wwb/gamebox/mobile/transfer/controller/TransferController.java
+++ mobile/java/so/wwb/gamebox/mobile/transfer/controller/TransferController.java
@@ -272,7 +272,7 @@ public class TransferController extends WalletBaseController {
         if (playerTransferVo.getResult().getTransferAmount() <= 0) {
             return getErrorMessage(TransferResultStatusEnum.TRANSFER_ERROR_AMOUNT.getCode(), playerTransferVo.getResult().getApiId());
         }
-        if (isDemo()) {//如果是demo，不让转账
+        if (!ParamTool.isAllowTransfer(SessionManager.getSiteId())) {//站点是否允许转账
             return getErrorMessage(TransferResultStatusEnum.TRANSFER_SWITCH_CLOSE.getCode(), playerTransferVo.getResult().getApiId());
         }
         Integer apiId = playerTransferVo.getResult().getApiId();
@@ -298,16 +298,6 @@ public class TransferController extends WalletBaseController {
      */
     private boolean isMaintain(Api api, SiteApi siteApi) {
         return GameStatusEnum.DISABLE.getCode().equals(api.getSystemStatus()) || GameStatusEnum.MAINTAIN.getCode().equals(api.getSystemStatus()) || GameStatusEnum.DISABLE.getCode().equals(siteApi.getSystemStatus()) || GameStatusEnum.MAINTAIN.getCode().equals(siteApi.getSystemStatus());
-    }
-
-    /**
-     * 判断站点是否是demo
-     *
-     * @return
-     */
-    private boolean isDemo() {
-        SysParam sysParam = ParamTool.getSysParam(SiteParamEnum.IS_DEMO);
-        return sysParam != null && sysParam.getParamValue().equals("true");
     }
 
     /**
