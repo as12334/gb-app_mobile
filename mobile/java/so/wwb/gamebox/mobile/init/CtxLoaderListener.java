521791c19ce7ef8171c26d68cf3cf16cbf33e8ee
tony <tony@wwb.so>
2017-12-01 19:45:35 +0800

    1.调整Web的关闭为异步，保留服务的关闭为同步

--- mobile/java/so/wwb/gamebox/mobile/init/CtxLoaderListener.java
+++ mobile/java/so/wwb/gamebox/mobile/init/CtxLoaderListener.java
@@ -2,13 +2,15 @@ package so.wwb.gamebox.mobile.init;
 
 import so.wwb.gamebox.web.init.CommonCtxLoaderListener;
 
+import javax.servlet.ServletContextEvent;
+
 /**
  * Created by Kevice on 2015/3/23 0023.
  */
 public class CtxLoaderListener extends CommonCtxLoaderListener {
     @Override
-    public void customInit() throws Exception {
-        super.customInit();
+    public void contextInitialized(ServletContextEvent event) {
+        super.contextInitialized(event);
         PassportContextPath="";
     }
 }
