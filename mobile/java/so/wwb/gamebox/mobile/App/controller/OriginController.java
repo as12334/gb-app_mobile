abf9b7ab37f0af75c6c2fe9ec1c05e868d7a7308
ed <edon@dawoo.com>
2017-12-25 11:41:04 +0800

    新增app接口首页数据

--- mobile/java/so/wwb/gamebox/mobile/App/controller/OriginController.java
+++ mobile/java/so/wwb/gamebox/mobile/App/controller/OriginController.java
@@ -1,10 +1,13 @@
 package so.wwb.gamebox.mobile.App.controller;
 
 
+import org.soul.commons.collections.ListTool;
+import org.soul.commons.collections.MapTool;
 import org.soul.commons.data.json.JsonTool;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import so.wwb.gamebox.mobile.controller.BaseApiController;
 import so.wwb.gamebox.model.company.site.po.SiteApiTypeRelationI18n;
 import so.wwb.gamebox.model.master.content.po.CttAnnouncement;
@@ -22,38 +25,38 @@ import java.util.Map;
 @Controller
 @RequestMapping("/origin")
 public class OriginController extends BaseApiController {
+    private final String version = "app_01";
+    Map<String,Object> map = MapTool.newHashMap();
 
     @RequestMapping("/mainIndex")
-    public String mainIndex(Model model, HttpServletRequest request, Integer skip, String path) {
-        Map<String,Object> map = new HashMap<>();
-        Map<String,Object> mapJson = new HashMap<>();
-        //查询banner
-        List<Map> bannerList = getCarousel(request, CarouselTypeEnum.CAROUSEL_TYPE_PHONE.getCode());
-        //查询公告
-        List<CttAnnouncement> announcementList = getAnnouncement();
-        //获取游戏及种类
-        Map<Integer, List<SiteApiTypeRelationI18n>> siteApiRelationMap = getSiteApiRelationI18n(model);
-
+    @ResponseBody
+    public String mainIndex(Model model, HttpServletRequest request) {
+        Map<String,Object> mapJson = MapTool.newHashMap();
 
         //浮动图
-        List<Map> floatList = new ArrayList<>();
+        List<Map> floatList = ListTool.newArrayList();
         showMoneyActivityFloat(floatList);
 
-        map.put("banner", bannerList);
-        map.put("announcement", announcementList);
-        map.put("siteApiRelation", siteApiRelationMap);
+        map.put("banner", getCarousel(request, CarouselTypeEnum.CAROUSEL_TYPE_PHONE.getCode()));
+        map.put("announcement", getAnnouncement());
+        map.put("siteApiRelation", getSiteApiRelationI18n(model));
         map.put("activity", floatList);
 
         mapJson.put("error", 0);
         mapJson.put("code", null);
         mapJson.put("msg", "请求成功");
-        mapJson.put("version", "app_01");
+        mapJson.put("version", version);
         mapJson.put("data", map);
 
-        String jsonStr = JsonTool.toJson(map);
-        return jsonStr;
+        return JsonTool.toJson(mapJson);
     }
 
+    @RequestMapping("/getCarouse")
+    @ResponseBody
+    public String getCarouse(HttpServletRequest request){
+        map.put("banner", getCarousel(request, CarouselTypeEnum.CAROUSEL_TYPE_PHONE.getCode()));
+        return null;
+    }
 
     @Override
     protected String getDemoIndex() {
