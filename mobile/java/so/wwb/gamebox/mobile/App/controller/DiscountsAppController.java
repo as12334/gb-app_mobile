d3be7cd99e6f33e38a56809db187d75f318f6b28
legend <legend@wwb.so>
2018-01-07 11:12:40 +0800

    删掉没有用到的import

--- mobile/java/so/wwb/gamebox/mobile/App/controller/DiscountsAppController.java
+++ mobile/java/so/wwb/gamebox/mobile/App/controller/DiscountsAppController.java
@@ -1,18 +1,14 @@
 package so.wwb.gamebox.mobile.App.controller;
 
-import org.soul.commons.collections.MapTool;
 import org.soul.commons.data.json.JsonTool;
-import org.soul.commons.lang.string.StringTool;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import so.wwb.gamebox.mobile.App.common.CommonApp;
-import so.wwb.gamebox.mobile.App.enums.AppErrorCodeEnum;
 import so.wwb.gamebox.mobile.App.model.AppModelVo;
 import so.wwb.gamebox.mobile.controller.BaseDiscountsController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 /**
  * Created by legend on 17-12-31.
@@ -20,8 +16,6 @@ import java.util.Map;
 @Controller
 @RequestMapping("/discountsOrigin")
 public class DiscountsAppController extends BaseDiscountsController{
-    private final String version = "app_01";
-    Map<String, Object> mapJson = MapTool.newHashMap();
 
     /**
      * 获取优惠活用
