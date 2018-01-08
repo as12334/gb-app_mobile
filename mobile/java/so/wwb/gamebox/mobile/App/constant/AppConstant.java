a5ab5b3799ee25d3b3c24d046a6ef77cdcb47d51
ed <edon@dawoo.com>
2018-01-08 10:10:53 +0800

    修改app 常量

--- mobile/java/so/wwb/gamebox/mobile/App/constant/AppConstant.java
+++ mobile/java/so/wwb/gamebox/mobile/App/constant/AppConstant.java
@@ -1,13 +1,13 @@
 package so.wwb.gamebox.mobile.App.constant;
 
 public interface AppConstant {
-    String appVersion = "app_01";
+    String appVersion = "app_01";//版本号
 
-    String SplitRegex = ",";
+    String SplitRegex = ",";//String转数据
 
-    String targetRegex = "x";
+    String targetRegex = "x";//匹配正则替换
 
-    int appErrorTimes = 5;
+    int appErrorTimes = 5;//安全码错误次数
 
     int TIME_INTERVAL = -30;
 
