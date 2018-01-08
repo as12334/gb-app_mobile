227b09a8b5b821724b61199550b873521b80853d
legend <legend@wwb.so>
2018-01-08 10:21:32 +0800

    加注释

--- mobile/java/so/wwb/gamebox/mobile/App/constant/AppConstant.java
+++ mobile/java/so/wwb/gamebox/mobile/App/constant/AppConstant.java
@@ -9,10 +9,10 @@ public interface AppConstant {
 
     int appErrorTimes = 5;//安全码错误次数
 
-    int TIME_INTERVAL = -30;
+    int TIME_INTERVAL = -30; //设置时间区间的最小时间
 
-    int DEFAULT_TIME = 1;
+    int DEFAULT_TIME = 1; //默认时间
 
-    int PROMO_RECORD_DAYS = -7;
+    int PROMO_RECORD_DAYS = -7; //设置一周前
     int RECOMMEND_DAYS = -1;
 }
