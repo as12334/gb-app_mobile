db235782b54c570238a700bf0eeaf4b12736239b
ed <edon@dawoo.com>
2018-02-02 11:41:37 +0800

    修改mobile-api接口返回数据格式

--- mobile-api/java/so/wwb/gamebox/mobile/app/model/AppModelVo.java
+++ mobile-api/java/so/wwb/gamebox/mobile/app/model/AppModelVo.java
@@ -2,11 +2,14 @@ package so.wwb.gamebox.mobile.app.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.soul.commons.data.json.JsonTool;
+import so.wwb.gamebox.mobile.app.constant.AppConstant;
 
-public class AppModelVo {
-    private Integer error;
+import java.io.Serializable;
+
+public class AppModelVo implements Serializable{
+
     private Integer code;
-    private String msg;
+    private String message;
     private Object data;
     private String version;
 
@@ -14,45 +17,73 @@ public class AppModelVo {
 
     }
 
-    public AppModelVo(Integer error, Integer code, String msg, Object data, String version) {
-        this.error = error;
+    public AppModelVo(Integer code, String message) {
         this.code = code;
-        this.msg = msg;
+        this.message = message;
+        this.version = AppConstant.APP_VERSION;
+    }
+
+    public AppModelVo(Integer code, String message,Object data, String version) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.version = AppConstant.APP_VERSION;
+    }
+
+    /**
+     * @deprecated
+     * @param error
+     * @param code
+     * @param message
+     * @param data
+     * @param version
+     */
+    public AppModelVo(Integer error, Integer code, String message, Object data, String version) {
+        this.code = code;
+        this.message = message;
         this.data = data;
         this.version = version;
     }
 
+
+    @JsonIgnore
+    public static String getAppModeVoJson(Integer code, String message, Object data, String version) {
+        return JsonTool.toJson(new AppModelVo(code, message, data, version));
+    }
+
+    /**
+     * @deprecated
+     * @param error
+     * @param code
+     * @param data
+     * @param
+     * @see #getAppModeVoJson(AppModelVo)
+     * @see #AppModelVo(Integer, String)
+     * @see #getAppModeVoJson(Integer, String, Object, String)
+     */
     @JsonIgnore
     public static String getAppModeVoJson(Integer error, Integer code, String msg, Object data, String version) {
         return JsonTool.toJson(new AppModelVo(error, code, msg, data, version));
     }
 
-    public Integer getError() {
-        return error;
+    public static String getAppModeVoJson(AppModelVo appModelVo) {
+        return JsonTool.toJson(appModelVo);
     }
 
-    public void setError(Integer error) {
-        this.error = error;
+    public Integer getCode() {
+        return code;
     }
 
     public void setCode(Integer code) {
         this.code = code;
     }
 
-    public int getCode() {
-        return code;
+    public String getMessage() {
+        return message;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public String getVersion() {
