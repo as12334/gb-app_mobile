94d8c9763cd778ec411a7d1331d6d2aa4c57a77d
cherry <cherry1012@mail.com>
2018-02-13 11:16:51 +0800

    ==修改mobile-v3 H5转账

--- mobile/java/so/wwb/gamebox/mobile/transfer/controller/TransferController.java
+++ mobile/java/so/wwb/gamebox/mobile/transfer/controller/TransferController.java
@@ -90,7 +90,7 @@ public class TransferController extends WalletBaseController {
             PlayerTransferVo playerTransferVo = new PlayerTransferVo();
             playerTransferVo.getSearch().setUserId(SessionManager.getUserId());
             model.addAttribute("transferPendingAmount", ServiceSiteTool.playerTransferService().queryProcessAmount(playerTransferVo));
-            return "/transfer/TransferIndex";
+            return TRANSFER_INDEX_URL;
 
         }
     }
