dd86526cb4e51677a7b4740040b3c8c0240c87fe
legend <legend@wwb.so>
2018-03-13 17:14:14 +0800

    推荐好友，分享记录 数据显示不出来的问题

--- mobile/java/so/wwb/gamebox/mobile/controller/IndexController.java
+++ mobile/java/so/wwb/gamebox/mobile/controller/IndexController.java
@@ -66,6 +66,7 @@ import so.wwb.gamebox.web.common.SiteCustomerServiceHelper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -188,7 +189,7 @@ public class IndexController extends BaseApiController {
      */
     @RequestMapping("/recommend")
     @Upgrade(upgrade = true)
-    public String recommend(Model model, PlayerRecommendAwardListVo listVo) {
+    public String recommend(Model model, PlayerRecommendAwardListVo listVo, HttpServletRequest request) {
         UserPlayerVo userPlayerVo = new UserPlayerVo();
         userPlayerVo.getSearch().setId(SessionManager.getUserId());
         userPlayerVo = ServiceSiteTool.userPlayerService().get(userPlayerVo);
@@ -235,6 +236,11 @@ public class IndexController extends BaseApiController {
         model.addAttribute("recommend", ServiceSiteTool.playerRecommendAwardService().searchRewardUserAndBonus(playerVo));
         //活动规则
         model.addAttribute("activityRules", Cache.getSiteI18n(SiteI18nEnum.MASTER_RECOMMEND_RULE).get(SessionManager.getLocale().toString()).getValue());
+        //是否是ajax请求
+        if (ServletTool.isAjaxSoulRequest(request)) {
+            return "/recommend/RecommendList";
+        }
+
         return "/recommend/Recommend";
     }
 
@@ -247,6 +253,7 @@ public class IndexController extends BaseApiController {
         for (PlayerRecommendAwardRecord record : records){
             record.setRecommendUserName(StringTool.overlayString(record.getRecommendUserName()));
             record.setStatus(SysUserStatus.enumOf(record.getStatus()).getTrans());
+            record.setRewardAmount(record.getRewardAmount() == null ? BigDecimal.ZERO : record.getRewardAmount());
         }
         return records;
     }
@@ -259,7 +266,8 @@ public class IndexController extends BaseApiController {
     private void initSearchDate(PlayerRecommendAwardListVo listVo) {
         if (listVo.getSearch().getStartTime() == null) {
             listVo.getSearch().setStartTime(SessionManager.getDate().addDays(DEFAULT_MIN_TIME));
-        } else if (listVo.getSearch().getEndTime() == null) {
+        }
+        if (listVo.getSearch().getEndTime() == null) {
             listVo.getSearch().setEndTime(SessionManager.getDate().getNow());
         }
 
