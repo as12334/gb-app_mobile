25bd96d4d6874fa606ee8fb598b3aff67b097257
ed <edon@dawoo.com>
2018-03-20 16:42:03 +0800

    修改mobile-api接口取款验证安全密码错误次数

--- mobile-api/java/so/wwb/gamebox/mobile/app/controller/WithdrawAppController.java
+++ mobile-api/java/so/wwb/gamebox/mobile/app/controller/WithdrawAppController.java
@@ -231,6 +231,7 @@ public class WithdrawAppController extends BaseWithDrawController {
      * @return
      */
     private String verifySafePwdErrorTime(String password, String code, Map tokenMap) {
+        SessionManager.refreshUser();
         SysUser user = SessionManager.getUser();
         Map<String, Object> map = initSecurityPwdMap();
         Boolean captcha = (Boolean) map.get(KEY_CAPTCHA);
