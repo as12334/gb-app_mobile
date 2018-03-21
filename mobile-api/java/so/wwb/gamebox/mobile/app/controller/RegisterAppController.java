4c09bb1337ad946066d35e840ac0b5701d554bf1
ed <edon@dawoo.com>
2018-03-21 21:01:19 +0800

    修改mobile-api注册下拉选项

--- mobile-api/java/so/wwb/gamebox/mobile/app/controller/RegisterAppController.java
+++ mobile-api/java/so/wwb/gamebox/mobile/app/controller/RegisterAppController.java
@@ -5,25 +5,35 @@ import org.soul.commons.bean.Pair;
 import org.soul.commons.collections.CollectionTool;
 import org.soul.commons.collections.MapTool;
 import org.soul.commons.data.json.JsonTool;
+import org.soul.commons.dict.DictTool;
 import org.soul.commons.lang.DateTool;
+import org.soul.commons.lang.string.I18nTool;
 import org.soul.commons.lang.string.StringTool;
 import org.soul.commons.log.Log;
 import org.soul.commons.log.LogFactory;
 import org.soul.model.ip.IpBean;
 import org.soul.model.security.privilege.po.SysUser;
+import org.soul.model.sys.po.SysDict;
 import org.soul.model.sys.po.SysParam;
+import org.soul.web.session.SessionManagerBase;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import so.wwb.gamebox.mobile.app.enums.AppErrorCodeEnum;
 import so.wwb.gamebox.mobile.app.model.AppModelVo;
 import so.wwb.gamebox.mobile.session.SessionManager;
+import so.wwb.gamebox.model.DictEnum;
+import so.wwb.gamebox.model.Module;
 import so.wwb.gamebox.model.ParamTool;
 import so.wwb.gamebox.model.SiteParamEnum;
 import so.wwb.gamebox.model.common.RegisterConst;
+import so.wwb.gamebox.model.company.site.po.SiteCurrency;
+import so.wwb.gamebox.model.company.site.po.SiteLanguage;
 import so.wwb.gamebox.model.company.sys.po.SysSite;
 import so.wwb.gamebox.model.master.setting.enums.FieldSortEnum;
+import so.wwb.gamebox.model.master.setting.enums.SiteCurrencyEnum;
 import so.wwb.gamebox.model.master.setting.po.FieldSort;
+import so.wwb.gamebox.web.SessionManagerCommon;
 import so.wwb.gamebox.web.cache.Cache;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,7 +43,7 @@ import static so.wwb.gamebox.mobile.app.constant.AppConstant.APP_VERSION;
 
 @Controller
 @RequestMapping("/registerOrigin")
-public class RegisterAppController{
+public class RegisterAppController {
     private static final Log LOG = LogFactory.getLog(RegisterAppController.class);
 
     /*注册设置和页面设定name映射*/
@@ -64,7 +74,7 @@ public class RegisterAppController{
      */
     @RequestMapping(value = "/getRegisterInfo")
     @ResponseBody
-    public String getRegisterInfo(HttpServletRequest request){
+    public String getRegisterInfo(HttpServletRequest request) {
         return AppModelVo.getAppModeVoJson(true, AppErrorCodeEnum.SUCCESS.getCode(),
                 AppErrorCodeEnum.SUCCESS.getMsg(),
                 getRegisterData(request),
@@ -119,7 +129,97 @@ public class RegisterAppController{
         map.put("isEmail", isValid(SiteParamEnum.SETTING_REG_SETTING_MAIL_VERIFCATION));
         //手机验证
         map.put("isPhone", isValid(SiteParamEnum.SETTING_REG_SETTING_PHONE_VERIFCATION));
+        map.put("selectOption", optionText(request));
         return map;
+    }
+
+    /**
+     * 下拉框选项内容
+     *
+     * @param request
+     * @return
+     */
+    public Map optionText(HttpServletRequest request) {
+        Map map = new HashMap();
+        DictEnum dictEnum = null;
+        //主货币
+        map.put("mainCurrency", currencyOption());
+        //性别
+        dictEnum = DictEnum.COMMON_SEX;
+        map.put("sex", optionText(dictEnum));
+        //语言
+        map.put("defaultLocale", getAvailableLanguage());
+        //密保
+        dictEnum = DictEnum.SETTING_MASTER_QUESTIONS;
+        map.put("securityIssues", optionText(dictEnum));
+        return map;
+    }
+
+    private List<Map<String, String>> getAvailableLanguage() {
+        Map<String, SiteLanguage> languageMap = Cache.getAvailableSiteLanguage();
+        List<SiteLanguage> languageList = new ArrayList<>(languageMap.values());
+        List<Map<String, String>> mapList = new ArrayList<>(languageList.size());
+            /*取字典国际化*/
+        Map i18nMap = I18nTool.getDictsMap(SessionManagerCommon.getLocale().toString())
+                .get(DictEnum.COMMON_LANGUAGE.getModule().getCode())
+                .get(DictEnum.COMMON_LANGUAGE.getType());
+
+        if (!languageList.isEmpty()) {
+            for (SiteLanguage siteLanguage : languageList) {
+                String lang = StringTool.substringBefore(i18nMap.get(siteLanguage.getLanguage()).toString(), "#");
+                Map<String, String> map = new HashMap<>(2, 1f);
+                map.put("text", lang);
+                map.put("value", siteLanguage.getLanguage());
+                mapList.add(map);
+            }
+        }
+        return mapList;
+    }
+
+    /**
+     * 获取下拉选项
+     *
+     * @param dictEnum
+     * @return
+     */
+    private List<Map<String, String>> optionText(DictEnum dictEnum) {
+        if (dictEnum != null) {
+            Map<String, SysDict> dicts = DictTool.get(dictEnum);
+            List<Map<String, String>> mapList = new ArrayList<>(dicts.size());
+            Map<String, String> i18nMap = I18nTool.getDictsMap(SessionManagerBase.getLocale().toString()).get(dictEnum.getModule().getCode()).get(dictEnum.getType());
+            if (i18nMap != null) {
+                for (SysDict sysDict : dicts.values()) {
+                    Map<String, String> map = new HashMap<>(2, 1f);
+                    map.put("text", i18nMap.get(sysDict.getDictCode()));
+                    map.put("value", sysDict.getDictCode());
+                    mapList.add(map);
+                }
+            }
+            return mapList;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取货币下拉框
+     *
+     * @return
+     */
+    private List<Map<String, String>> currencyOption() {
+        List<SiteCurrency> siteCurrencies = new ArrayList<>(Cache.getSiteCurrency().values());
+        List<Map<String, String>> mapList = new ArrayList<>();
+        Map<String, String> map;
+        Map<String, String> i18nMap = I18nTool.getDictsMap(SessionManagerBase.getLocale().toString()).get(Module.COMMON.getCode()).get(DictEnum.COMMON_CURRENCY.getType());
+        for (SiteCurrency siteCurrency : siteCurrencies) {
+            if (SiteCurrencyEnum.NORMAL.getCode().equals(siteCurrency.getStatus())) {
+                map = new HashMap<>(2, 1f);
+                map.put("text", i18nMap.get(siteCurrency.getCode()));
+                map.put("value", siteCurrency.getCode());
+                mapList.add(map);
+            }
+        }
+        return mapList;
     }
 
     /**
