113ad0628587d7317fcf0f96cd40ef712ee55af8
cherry <cherry1012@mail.com>
2018-04-13 12:14:00 +0800

    修改token验证

--- mobile/java/so/wwb/gamebox/mobile/transfer/controller/TransferController.java
+++ mobile/java/so/wwb/gamebox/mobile/transfer/controller/TransferController.java
@@ -425,9 +425,6 @@ public class TransferController extends WalletBaseController {
             msg = LocaleTool.tranMessage(_Module.COMMON, MessageI18nConst.SAVE_FAILED);
         }
         map.put("msg", msg);
-        if (!isSuccess) {
-            map.put(TokenHandler.TOKEN_VALUE, TokenHandler.generateGUID());
-        }
         return map;
     }
 
