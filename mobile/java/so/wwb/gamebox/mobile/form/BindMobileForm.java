b8028361d7a30466cdcc84191378a522208cbe6f
snake <1113214509>
2018-05-04 12:04:43 +0800

    修改mobile-v3优惠记录

--- mobile/java/so/wwb/gamebox/mobile/form/BindMobileForm.java
+++ mobile/java/so/wwb/gamebox/mobile/form/BindMobileForm.java
@@ -1,11 +1,13 @@
 package so.wwb.gamebox.mobile.form;
 
+import org.hibernate.validator.constraints.NotBlank;
 import org.soul.commons.query.enums.Operator;
 import org.soul.commons.validation.form.constraints.Depends;
 import org.soul.commons.validation.form.constraints.Remote;
 import org.soul.commons.validation.form.support.AndOr;
 import org.soul.web.support.IForm;
 import so.wwb.gamebox.mobile.common.consts.FormValidRegExps;
+import so.wwb.gamebox.mobile.controller.HelpCenterController;
 import so.wwb.gamebox.mobile.passport.controller.SignUpController;
 import so.wwb.gamebox.web.common.controller.VerificationCodeController;
 
@@ -17,25 +19,25 @@ import javax.validation.constraints.Pattern;
 public class BindMobileForm implements IForm {
 
     //手机号
-    private String phone_contactValue;
+    private String search_contactValue;
 
     //短信验证码
     private String $phoneCode;
 
-    @Depends(message = "Register.phone.notBlank", operator = {Operator.IN, Operator.IS_NOT_EMPTY}, value = {"110", ""}, property = {"$requiredJson", "$checkPhone"}, andOr = AndOr.OR)
+    @NotBlank(message = "Register.phone.notBlank")
     @Pattern(regexp = FormValidRegExps.NUMBER_PHONE, message = "Register.phone.format")
-    @Remote(message = "Register.phone.exist", checkClass = SignUpController.class, checkMethod = "checkPhoneExist")
-    public String getPhone_contactValue() {
-        return phone_contactValue;
+    @Remote(message = "Register.phone.exist", checkClass = HelpCenterController.class, checkMethod = "checkPhoneExist")
+    public String getSearch_contactValue() {
+        return search_contactValue;
     }
 
-    public void setPhone_contactValue(String phone_contactValue) {
-        this.phone_contactValue = phone_contactValue;
+    public void setSearch_contactValue(String phone_contactValue) {
+        this.search_contactValue = phone_contactValue;
     }
 
 
-    @Depends(message = "Register.phoneCode.notBlank", operator = Operator.IS_NOT_EMPTY, property = "$checkPhone")
-    @Remote(message = "Register.phoneCode.remote", checkClass = VerificationCodeController.class, checkMethod = "checkPhoneCode", additionalProperties = "phone.contactValue")
+    @NotBlank(message = "Register.phoneCode.notBlank")
+    @Remote(message = "Register.phoneCode.remote", checkClass = HelpCenterController.class, checkMethod = "checkPhoneCode", additionalProperties = "search.contactValue")
     public String get$phoneCode() {
         return $phoneCode;
     }
